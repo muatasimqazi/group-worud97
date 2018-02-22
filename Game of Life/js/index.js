67df8910744dcc7dfc50fca950d2f41719d61e31
Ben Celsi <bencelsi@uw.edu>
2018-02-21 21:41:07 -0800

    added canvas/began menu

--- Game of Life/js/index.js
+++ Game of Life/js/index.js
@@ -1,65 +1,62 @@
-let CELLS = document.querySelector("#cells");
-let DIM = 60;
+let DIM = 200;
+let PIX = 2;
+let CAN = qs("canvas")
+let CTX = CAN.getContext("2d");
+let grid = [];
+let timer = null;
+let boundless = true;
+let speed = 20;
+CAN.setAttribute("width", DIM*PIX + "px");
+CAN.setAttribute("height", DIM*PIX + "px");
 
-let neighbors = [[1,1],[1,0],[1,-1],[0,-1],[-1,-1],[-1,0],[-1,1],[0,1]]; //classic
-//let neighbors = [[1,1],[1,-1],[-1,-1],[-1,1]]; //diagonals only
-//let neighbors = [[1,1],[1,-1],[-1,-1],[-1,1],[2,2],[2,-2],[-2,-2],[-2,2]]; //diagonal star
-//let neighbors = [[0,1],[0,-1],[0,2],[0,-2],[0,3],[0,-3]]; //line out
-
-let grid = []
 for (let y = 0; y < DIM; y++) {
     let row = [];
-    for (let x = 0; x < DIM; x++) {
-        row[x] = [x, y, Math.floor(Math.random()*2)];
-    }
-    grid[y] = row;  
+    for (let x = 0; x < DIM; x++) {    
+        row[x] = [x, y, 0]
+    };
+    grid[y] = row
 }
 
-function getCell(x, y){ 
-    if (x >= 0 && x < DIM && y >= 0 && y < DIM){
-        return (grid [y] [x] [2])
-    } else {return 0;}
+function qs(q){
+    return document.querySelector(q);
+}
+
+function makeGrid(type){
+    let val = type;
+    for (let y = 0; y < DIM; y++) {
+        for (let x = 0; x < DIM; x++) {
+            if (type === 2){
+                val = (Math.floor(Math.random()*2))}
+            setCell(x, y, val);
+        }
+    }
+    render();
+} makeGrid(2)
+
+
+function mod(n, m) {
+    return ((n % m) + m) % m;
+} function getCell(x, y){
+    if (boundless) {
+        return (grid [mod (y, DIM-1)] [mod(x, DIM-1)] [2]);
+    } else if (x >= 0 && x < DIM && y >= 0 && y < DIM) {
+        return (grid [y] [x] [2]);
+    } else {
+        return 0;
+    }
 }
 
 function setCell(x, y, state){ 
-    if (x >= 0 && x < DIM && y >= 0 && y < DIM){
-        grid [y] [x] [2]= state;
-    } else {alert("uh oh");}
+    grid [y] [x] [2] = state;
 }
 
-for (let y = 0; y < DIM; y++){
-    let row = document.createElement("div");
-    for (let x = 0; x < DIM; x++){
-        let cell = document.createElement("button");
-        cell.classList.add("cell")
-        cell.id=("x"+x+"y"+y)
-        //cell.textContent = getNeighbors(x,y)
-        cell.addEventListener("click", function(){
-            if (this.classList.contains("c1")){
-                cell.classList.add("c0");
-                cell.classList.remove("c1");
-                setCell(x, y, 0)
-            } else {
-                cell.classList.add("c1");
-                cell.classList.remove("c0");
-                setCell(x, y, 1)
-            }
-        })
-        row.appendChild(cell);
-    }
-    CELLS.appendChild(row);
-}
-
-function render () {
-    for (let y = 0; y < DIM; y++){
-        for (let x = 0; x < DIM; x++){
-            let cell = document.querySelector("#x"+x+"y"+y)
-            if (getCell(x, y) === 1){
-                cell.classList.add("c1");
-                cell.classList.remove("c0");
-            } else {
-                cell.classList.add("c0");
-                cell.classList.remove("c1");
+function render() { //draw the contents of the grid onto a canvas
+    CTX.clearRect(0, 0, DIM*PIX, DIM*PIX); //this should clear the canvas ahead of each redraw
+    for (var y = 0; y < DIM; y++) { //iterate through rows
+        for (var x = 0; x < DIM; x++) { //iterate through columns
+            if (getCell(x,y) === 1) {
+                CTX.fillStyle = "#0342CC";
+                CTX.fillRect(x*PIX, y*PIX, PIX, PIX);
             }
         }
     }
@@ -86,25 +83,112 @@ function getNeighbors(x, y) {
 function rule (cell) {
     let state = cell [2];
     let n = getNeighbors(cell[0], cell[1]);
-    if (state === 1){
-        if (n == 2 || n == 3){
-            return 1
-        } else {
-            return 0
-        }
+    if (state == 1){
+        return (onRule [n]);
     } else {
-        if (n == 3) {
-            return 1
-        } else {
-            return 0
-        }
+        return (offRule [n]);
     }
 }
+
+//let offRule = [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]//classic
+//let onRule = [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+//let offRule = [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]//boxy
+//let onRule = [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+//let offRule = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]//dotty
+//let onRule = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+//let offRule = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]//retro
+//let onRule = [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]
+//let offRule = [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]//woozily
+//let onRule = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+//let offRule = [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]//maze
+//let onRule = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+//let offRule = [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]//retro
+//let onRule = [1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+//let offRule = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]//cowy
+//let onRule = [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+//let offRule = [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]//thick
+//let onRule = [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+let offRule = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]//empty
+let onRule = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+//let neighbors = [[1,1],[1,0],[1,-1],[0,-1],[-1,-1],[-1,0],[-1,1],[0,1]]; //classic
+//let neighbors = [[1,1],[1,-1],[-1,-1],[-1,1]]; //diagonals only
+//let neighbors = [[1,1],[1,-1],[-1,-1],[-1,1],[2,2],[2,-2],[-2,-2],[-2,2]]; //diagonal star
+//let neighbors = [[0,1],[0,-1],[0,2],[0,-2],[0,3],[0,-3]]; //line out
+let neighbors = [[0,1],[0,-1],[-1,0],[1,0],[2,0],[0,2],[-2,0],[0,-2]];// straight star
+//let neighbors = [[2,1],[2,0],[1,-3],[0,-1],[1,-1],[1,0],[4,4],[0,-2]]; //random
+//let neighbors = [[1,1],[2,2],[-1,1],[-2,2],[0,1],[0,2],[0,1]]; //arrow
+//let neighbors = [[1,1],[2,2],[-1,1],[-2,2],[0,1],[0,2],[0,2]]; //arrow 2
+//let neighbors = [[1,1],[2,2],[-1,1],[-2,2],[0,1],[0,2],[0,3]]; //arrow 3
+//let neighbors = [[1,1],[2,2],[-1,1],[-2,2],[0,10],[0,20],[0,40]]; //arrow 4
+//let neighbors = [[0,2],[1,2],[-1,2],[0,-2],[1,-2],[-1,-2],[-2,0],[-2,-1],[-2,1],[2,0],[2,-1],[2,1]] //box
+//combos- arrow + dotty, arrow+thick
+
+function makeRuleButtons(rule) {
+    for (let i = 0; i < 26; i++){
+        let btn = document.createElement("button")
+        btn.classList.add("rulebutton");
+        btn.textContent = i;
+        btn.addEventListener("click", function(){
+            let on = (onRule [i] === 1);
+            if (rule === 0){
+                on = (offRule [i] === 1);
+            }
+            let newRule = 0;
+            if (on) {         
+                this.style.backgroundColor = "white";
+            } else {
+                this.style.backgroundColor = "red";
+                newRule = 1;
+            }
+            if (rule === 0){
+                offRule [i] = newRule;
+            } else {
+                onRule [i] = newRule;
+            }
+            console.log(newRule+ "   " + on)
+        });
+        if (rule === 0) {
+            qs("#offrule").appendChild(btn)
+        } else {
+            qs("#onrule").appendChild(btn)
+        }
+    }  
+}
+
+makeRuleButtons(1);
+makeRuleButtons(0)
 
 document.onkeydown = function (e) {
     var keyCode = e.keyCode;
     if(e.keyCode == 50) {
-        console.log("step");
         step();
+    } else if (e.keyCode == 52){
+        
     }
 };
+
+qs("#boundless").addEventListener("click", function (){
+        boundless = this.checked;});
+
+qs("#step").addEventListener("click", function () {
+    if (timer === null) {    
+        step();}});
+
+qs("#start").addEventListener("click", function () {
+        if (timer === null) {
+            timer = setInterval(function() {
+                step();}, speed)}});
+
+qs("#stop").addEventListener("click", function (){
+        clearInterval(timer);
+        timer = null;});
+        
+qs("#random").addEventListener("click", function (){
+    makeGrid(2);});
+    
+qs("#clear").addEventListener("click", function (){
+    makeGrid(0);});
+
+qs("#fill").addEventListener("click", function (){
+    makeGrid(1);});
