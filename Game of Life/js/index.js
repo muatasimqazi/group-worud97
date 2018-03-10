eab9abb0e6c4151e1876778e9c3b9da7340b6e87
Muatasim Qazi <muatasimqazi@gmail.com>
2018-03-09 22:07:03 -0800

    fix ui for tetris and gol

--- Game of Life/js/index.js
+++ Game of Life/js/index.js
@@ -13,10 +13,10 @@ let color = 0;
 
 let offRule = [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 let onRule = [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-let nbrs = [[1,1],[1,0],[1,-1],[0,-1],[-1,-1],[-1,0],[-1,1],[0,1]];
+let nbrs = [[1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1], [0, 1]];
 
 let boundless = true;
-let resolutions = [[60, 10],[75, 8], [100, 6], [150, 4], [300,2]] //600px
+let resolutions = [[60, 10], [75, 8], [100, 6], [150, 4], [300, 2]] //600px
 let speeds = [400, 225, 150, 75, 0]
 let brushSizes = [0, 1, 3, 5, 10]
 let canvases = ["CLEAR", "FILL", "RANDOM", "CHECKERS", "STRIPES"]
@@ -30,40 +30,40 @@ let delay = speeds[speed];
 let onColor = "blue";
 let offColor = "white"
 
-document.querySelector("#toggle-drawer").addEventListener("click", function() {
+document.querySelector("#toggle-drawer").addEventListener("click", function () {
     document.body.classList.toggle("drawer-open");
 });
 
-document.querySelector("#content").addEventListener("click", function() {
+document.querySelector("#content").addEventListener("click", function () {
     document.body.classList.remove("drawer-open");
 });
 
-CAN.setAttribute("width", DIM*PIX + "px");
-CAN.setAttribute("height", DIM*PIX + "px");
-CAN.addEventListener("mousedown", function(e){
-    let x = Math.floor((e.pageX - CAN.offsetLeft)/PIX)
-    let y = Math.floor((e.pageY - CAN.offsetTop)/PIX)
-    console.log(x+ " " + y)
+CAN.setAttribute("width", DIM * PIX + "px");
+CAN.setAttribute("height", DIM * PIX + "px");
+CAN.addEventListener("mousedown", function (e) {
+    let x = Math.floor((e.pageX - CAN.offsetLeft) / PIX)
+    let y = Math.floor((e.pageY - CAN.offsetTop) / PIX)
+    console.log(x + " " + y)
     setCell(x, y, 1)
-    for (let i = x - brushSizes[brushSize]; i <= x + brushSizes[brushSize]; i++){
-        for (let j = y - brushSizes[brushSize]; j <= y + brushSizes[brushSize]; j++){
+    for (let i = x - brushSizes[brushSize]; i <= x + brushSizes[brushSize]; i++) {
+        for (let j = y - brushSizes[brushSize]; j <= y + brushSizes[brushSize]; j++) {
             if (brushMode === 0) { //fill
                 setCell(i, j, 1)
             } else if (brushMode === 1) { //clear
                 setCell(i, j, 0)
             } else if (brushMode === 2) { //invert
-                setCell(i, j, (getCell(i, j) === 0)? 1 : 0)
+                setCell(i, j, (getCell(i, j) === 0) ? 1 : 0)
             } else { //random
-                setCell(i, j, Math.floor(2*Math.random()))
+                setCell(i, j, Math.floor(2 * Math.random()))
             }
         }
     }
     render();
 })
-qs("#menu").style.width = DIM*PIX + "px"
-qs("#menu").style.height = DIM*PIX + "px"
+// qs("#menu").style.width = DIM*PIX + "px"
+// qs("#menu").style.height = DIM*PIX + "px"
 
-function qs(q){
+function qs(q) {
     return document.querySelector(q);
 }
 
@@ -71,49 +71,49 @@ function mod(n, m) {
     return ((n % m) + m) % m;
 }
 
-function makeGrid(){
+function makeGrid() {
     for (let y = 0; y < DIM; y++) {
-        let row = [];   
+        let row = [];
         for (let x = 0; x < DIM; x++) {
             let val = canvas;
             if (canvas === 2) {
-                val = Math.floor(Math.random()*2)
+                val = Math.floor(Math.random() * 2)
             } else if (canvas === 3) {
-                ((x-y) % 2 === 0)? val = 0 : val = 1
+                ((x - y) % 2 === 0) ? val = 0 : val = 1
             } else if (canvas === 4) {
-                (x % 2 === 0)? val = 0 : val = 1
+                (x % 2 === 0) ? val = 0 : val = 1
             }
             row[x] = [x, y, val]
         };
         grid[y] = row
     }
     render();
-} 
+}
 makeGrid();
 
 
-function getCell(x, y){
+function getCell(x, y) {
     if (boundless) {
-        return (grid [mod (y, DIM)] [mod(x, DIM)] [2]);
+        return (grid[mod(y, DIM)][mod(x, DIM)][2]);
     } else if (x >= 0 && x < DIM && y >= 0 && y < DIM) {
-        return (grid [y] [x] [2]);
+        return (grid[y][x][2]);
     } else {
         return 0;
     }
 }
 
-function setCell (x, y, state) {
-    grid [y] [x] [2] = state;
-}   
+function setCell(x, y, state) {
+    grid[y][x][2] = state;
+}
 
 function render() { //draw the contents of the grid onto a canvas   
     qs("#cells").style.backgroundColor = offColor;
-    CTX.clearRect(0, 0, DIM*PIX, DIM*PIX); //this should clear the canvas ahead of each redraw
+    CTX.clearRect(0, 0, DIM * PIX, DIM * PIX); //this should clear the canvas ahead of each redraw
     for (var y = 0; y < DIM; y++) { //iterate through rows
         for (var x = 0; x < DIM; x++) { //iterate through columns
             if (getCell(x, y) >= 1) {
                 CTX.fillStyle = onColor;
-                CTX.fillRect(x*PIX, y*PIX, PIX, PIX);
+                CTX.fillRect(x * PIX, y * PIX, PIX, PIX);
             }
         }
     }
@@ -121,7 +121,7 @@ function render() { //draw the contents of the grid onto a canvas
 
 render();
 
-function step(){
+function step() {
     grid = grid.map(row => row.map(cell =>
         [cell[0], cell[1], rule(cell)]))
     render();
@@ -130,19 +130,19 @@ function step(){
 function getNeighbors(x, y) {
     let n = 0
     for (let i = 0; i < nbrs.length; i++) {
-        if (getCell(x + nbrs [i] [0], y + nbrs[i] [1]) >= 1){
+        if (getCell(x + nbrs[i][0], y + nbrs[i][1]) >= 1) {
             n++;
         }
     }
     return n;
 }
 
-function rule (cell) {
-    let state = cell [2];
-    if (cell [2] === 1){
-        return (onRule [getNeighbors(cell[0], cell[1])]);
+function rule(cell) {
+    let state = cell[2];
+    if (cell[2] === 1) {
+        return (onRule[getNeighbors(cell[0], cell[1])]);
     } else {
-        return (offRule [getNeighbors(cell[0], cell[1])]);
+        return (offRule[getNeighbors(cell[0], cell[1])]);
     }
 }
 
@@ -164,7 +164,7 @@ function rule (cell) {
 //let onRule = [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 //let offRule = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]//empty
 //let onRule = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
- //classic
+//classic
 //let nbrs = [[1,1],[1,-1],[-1,-1],[-1,1]]; //diagonals only
 //let nbrs = [[1,1],[1,-1],[-1,-1],[-1,1],[2,2],[2,-2],[-2,-2],[-2,2]]; //diagonal star
 //let nbrs = [[0,1],[0,-1],[0,2],[0,-2],[0,3],[0,-3]]; //line out
@@ -178,44 +178,44 @@ function rule (cell) {
 //combos- arrow + dotty, arrow+thick
 
 function makeRuleBtns(ruleArr) {
-    for (let i = 0; i < ruleArr.length; i++){
+    for (let i = 0; i < ruleArr.length; i++) {
         let btn = document.createElement("button")
         btn.classList.add("rulebutton");
         btn.textContent = i;
-        btn.addEventListener("click", function(){
-            let on = (ruleArr [i] === 1);
+        btn.addEventListener("click", function () {
+            let on = (ruleArr[i] === 1);
             let newRule = 0;
-            if (on) {         
+            if (on) {
                 this.style.backgroundColor = offColor;
-                ruleArr [i] = 0
+                ruleArr[i] = 0
             } else {
                 this.style.backgroundColor = onColor;
-                ruleArr [i] = 1
+                ruleArr[i] = 1
             }
         });
         if (ruleArr == onRule) {
-            btn.id =("onrb"+ i);
+            btn.id = ("onrb" + i);
             qs("#onrule").appendChild(btn)
         } else {
-            btn.id =("offrb"+ i);
+            btn.id = ("offrb" + i);
             qs("#offrule").appendChild(btn)
         }
     }
 }
 
 function renderRuleBtns(rule) {
-    for (let i = 0; i < rule.length; i++){
-        let btn = qs("#offrb"+i)
-        if (rule === onRule){
-            btn = qs("#onrb"+i)
+    for (let i = 0; i < rule.length; i++) {
+        let btn = qs("#offrb" + i)
+        if (rule === onRule) {
+            btn = qs("#onrb" + i)
         }
-        if (i > nbrs.length){
+        if (i > nbrs.length) {
             btn.disabled = true;
             btn.style.display = "none";
         } else {
             btn.disabled = false;
             btn.style.display = "inline";
-            if (rule [i] === 1){
+            if (rule[i] === 1) {
                 btn.style.backgroundColor = onColor
             } else {
                 btn.style.backgroundColor = offColor
@@ -229,28 +229,28 @@ makeRuleBtns(offRule);
 renderRuleBtns(onRule);
 renderRuleBtns(offRule);
 
-function makeNbrBtns(){
-    for (let y = -NBR_DIM; y <= NBR_DIM; y++){
+function makeNbrBtns() {
+    for (let y = -NBR_DIM; y <= NBR_DIM; y++) {
         let row = document.createElement("div")
         row.classList.add("nbrRow")
-        for (let x = -NBR_DIM; x <= NBR_DIM; x++){
+        for (let x = -NBR_DIM; x <= NBR_DIM; x++) {
             let btn = document.createElement("button")
-            btn.id = "nbrx"+x+"y"+y;
+            btn.id = "nbrx" + x + "y" + y;
             btn.classList.add("nbrBtn");
-            if (x !== 0 || y !== 0){
-                btn.addEventListener("click", function (){
+            if (x !== 0 || y !== 0) {
+                btn.addEventListener("click", function () {
                     let nbrIndex = -1
-                    for (let i = 0; i < nbrs.length; i++){
-                        if (nbrs[i][0] === x && nbrs[i][1] === y){
+                    for (let i = 0; i < nbrs.length; i++) {
+                        if (nbrs[i][0] === x && nbrs[i][1] === y) {
                             nbrIndex = i;
                             break;
                         }
                     }
-                    if (nbrIndex === -1){
-                        if (nbrs.length === 16){
-                            nbrs.splice(0, 1); 
+                    if (nbrIndex === -1) {
+                        if (nbrs.length === 16) {
+                            nbrs.splice(0, 1);
                         }
-                        nbrs.push([x,y]);
+                        nbrs.push([x, y]);
                     } else {
                         nbrs.splice(nbrIndex, 1);
                     }
@@ -266,19 +266,19 @@ function makeNbrBtns(){
 }
 
 makeNbrBtns();
-let q = [1,2,3,4,5,6,7]
-function renderNbrBtns(){
+let q = [1, 2, 3, 4, 5, 6, 7]
+function renderNbrBtns() {
     let nbrBtns = document.getElementsByClassName("nbrBtn");
-    for (let i = 0; i < nbrBtns.length; i++){
+    for (let i = 0; i < nbrBtns.length; i++) {
         nbrBtns[i].style.backgroundColor = "white";
     }
     for (let i = 0; i < nbrs.length; i++) {
-        qs("#nbrx"+nbrs[i][0]+"y"+nbrs[i][1]).style.backgroundColor = "black";
+        qs("#nbrx" + nbrs[i][0] + "y" + nbrs[i][1]).style.backgroundColor = "black";
     }
     qs("#nbrx0y0").style.backgroundColor = onColor;
 }
 
-function removeNbrBtn(x,y){
+function removeNbrBtn(x, y) {
     let tmpNbrs = nbrs.slice();
 }
 
@@ -287,29 +287,29 @@ let nbrNext = 0;
 
 
 qs("#boundless").addEventListener("click", function () {
-    boundless = !boundless  
+    boundless = !boundless
     this.innerHTML = "<strong>Boundless: </strong> " + ((boundless) ? "ON" : "OFF")
 });
 
 qs("#brushMode").addEventListener("click", function () {
-    brushMode = mod(brushMode+1, brushModes.length)
+    brushMode = mod(brushMode + 1, brushModes.length)
     this.innerHTML = "<strong>Brush Mode: </strong> " + brushModes[brushMode]
 });
 
 qs("#brushSize").addEventListener("click", function () {
-    brushSize = mod(brushSize+1, brushSizes.length)
+    brushSize = mod(brushSize + 1, brushSizes.length)
     this.innerHTML = "<strong>Brush Size: </strong>" + sizes[brushSize]
 });
 
 qs("#canvas").addEventListener("click", function () {
-    canvas = mod(canvas+1, canvases.length)
+    canvas = mod(canvas + 1, canvases.length)
     stop();
     makeGrid();
     this.innerHTML = "<strong>Set Canvas: </strong>" + canvases[canvas]
 });
 
 qs("#color").addEventListener("click", function () {
-    color = mod(color+1, colors.length)
+    color = mod(color + 1, colors.length)
     onColor = colors[color].toLowerCase()
     render();
     renderNbrBtns();
@@ -319,7 +319,7 @@ qs("#color").addEventListener("click", function () {
 });
 
 qs("#resolution").addEventListener("click", function () {
-    resolution = mod(resolution+1, resolutions.length)
+    resolution = mod(resolution + 1, resolutions.length)
     DIM = resolutions[resolution][0]
     PIX = resolutions[resolution][1]
     makeGrid();
@@ -329,7 +329,7 @@ qs("#resolution").addEventListener("click", function () {
 qs("#speed").addEventListener("click", function () {
     speed = mod(speed + 1, speeds.length);
     delay = speeds[speed];
-    if (timer != null) {   
+    if (timer != null) {
         stop();
         start();
     }
@@ -337,16 +337,30 @@ qs("#speed").addEventListener("click", function () {
 });
 
 qs("#step").addEventListener("click", function () {
-    if (timer === null) {    
-        step();}});
+    if (timer === null) {
+        step();
+    }
+});
 
 qs("#start").addEventListener("click", start);
-function start(){
+function start() {
     if (timer === null) {
-        timer = setInterval(function() {
-             step();}, delay)}}
+        timer = setInterval(function () {
+            step();
+        }, delay)
+    }
+}
 
 qs("#stop").addEventListener("click", stop);
-function stop(){
+function stop() {
     clearInterval(timer);
-    timer = null;}
+    timer = null;
+}
+
+// materialize CSS side nav
+$('.button-collapse').sideNav({
+    menuWidth: 300,
+    edge: 'left',
+    closeOnClick: true,
+    draggable: true,
+});
--- Tetris/js/app.js
+++ Tetris/js/app.js
@@ -2,47 +2,47 @@
 
 "use strict";
 
-document.querySelector("#toggle-drawer").addEventListener("click", function() {
+document.querySelector("#toggle-drawer").addEventListener("click", function () {
     document.body.classList.toggle("drawer-open");
 });
 
-document.querySelector("#content").addEventListener("click", function() {
+document.querySelector("#content").addEventListener("click", function () {
     document.body.classList.remove("drawer-open");
 });
 
 const TIME_SPAN = document.querySelector("#time");
 const CANVAS = document.getElementById("canvas");
 const CTX = CANVAS.getContext("2d");
-const DIR = {UP: 0, RIGHT: 1, DOWN: 2, LEFT: 3,  MIN: 0, MAX: 3};
-const SPEED = {start: 0.6, decrement: 0.005, min: 0.1};
+const DIR = { UP: 0, RIGHT: 1, DOWN: 2, LEFT: 3, MIN: 0, MAX: 3 };
+const SPEED = { start: 0.6, decrement: 0.005, min: 0.1 };
 const INVALID = {};
 
-let I = {size: 4, blocks: [0x0F00, 0x2222, 0x00F0, 0x4444], color: "#FAFAD2"};
-let J = {size: 3, blocks: [0x44C0, 0x8E00, 0x6440, 0x0E20], color: "#00FFFF"};
-let L = {size: 3, blocks: [0x4460, 0x0E80, 0xC440, 0x2E00], color: "#7FFFD4"};
-let O = {size: 2, blocks: [0xCC00, 0xCC00, 0xCC00, 0xCC00], color: "#FFC0CB"};
-let S = {size: 3, blocks: [0x06C0, 0x8C40, 0x6C00, 0x4620], color: "#E6E6FA"};
-let T = {size: 3, blocks: [0x0E40, 0x4C40, 0x4E00, 0x4640], color: "#FFA07A"};
-let Z = {size: 3, blocks: [0x0C60, 0x4C80, 0xC600, 0x2640], color: "#B0C4DE"};
+let I = { size: 4, blocks: [0x0F00, 0x2222, 0x00F0, 0x4444], color: "#FAFAD2" };
+let J = { size: 3, blocks: [0x44C0, 0x8E00, 0x6440, 0x0E20], color: "#00FFFF" };
+let L = { size: 3, blocks: [0x4460, 0x0E80, 0xC440, 0x2E00], color: "#7FFFD4" };
+let O = { size: 2, blocks: [0xCC00, 0xCC00, 0xCC00, 0xCC00], color: "#FFC0CB" };
+let S = { size: 3, blocks: [0x06C0, 0x8C40, 0x6C00, 0x4620], color: "#E6E6FA" };
+let T = { size: 3, blocks: [0x0E40, 0x4C40, 0x4E00, 0x4640], color: "#FFA07A" };
+let Z = { size: 3, blocks: [0x0C60, 0x4C80, 0xC600, 0x2640], color: "#B0C4DE" };
 
 let width = 10;
 let height = 20;
 
-let dx, dy,        
-    blocks,        
-    moves,       
-    playing,       
-    dt,            
-    current,      
-    next,          
-    score,         
-    vscore,        
-    lines,         
-    step;  
+let dx, dy,
+    blocks,
+    moves,
+    playing,
+    dt,
+    current,
+    next,
+    score,
+    vscore,
+    lines,
+    step;
 
-let timer; 
+let timer;
 let min = 0;
-let sec = 0; 
+let sec = 0;
 function showTime() {
     timer = setInterval(function () {
         TIME_SPAN.textContent = min + " min " + sec + " sec";
@@ -53,27 +53,27 @@ function showTime() {
         }
     }, 1000);
 }
-  
-function random(min, max) { 
-    return (min + (Math.random() * (max - min)));            
+
+function random(min, max) {
+    return (min + (Math.random() * (max - min)));
 }
-  
-function randomChoice(choices) { 
-    return choices[Math.round(random(0, choices.length-1))]; 
-}        
+
+function randomChoice(choices) {
+    return choices[Math.round(random(0, choices.length - 1))];
+}
 
 let tetros = [];
 function renderTetro() {
     if (tetros.length == 0) {
-        tetros = [I,I,I,J,J,J,L,L,L,O,O,O,S,S,S,T,T,T,Z,Z,Z];
-    }    
-    let type = tetros.splice(random(0, tetros.length-1), 1)[0];
-    return {type: type, dir: DIR.UP, x: Math.round(random(0, width - type.size)), y: 0};
+        tetros = [I, I, I, J, J, J, L, L, L, O, O, O, S, S, S, T, T, T, Z, Z, Z];
+    }
+    let type = tetros.splice(random(0, tetros.length - 1), 1)[0];
+    return { type: type, dir: DIR.UP, x: Math.round(random(0, width - type.size)), y: 0 };
 }
 
 function eachblock(type, x, y, dir, fn) {
     let bit, result, lines = 0, col = 0, blocks = type.blocks[dir];
-    for(bit = 0x8000; bit > 0; bit = bit >> 1) {
+    for (bit = 0x8000; bit > 0; bit = bit >> 1) {
         if (blocks & bit) {
             fn(x + col, y + lines);
         }
@@ -86,8 +86,8 @@ function eachblock(type, x, y, dir, fn) {
 
 function filled(type, x, y, dir) {
     let result = false
-    eachblock(type, x, y, dir, function(x, y) {
-        if ((x < 0) || (x >= width) || (y < 0) || (y >= height) || getBlock(x,y)) {
+    eachblock(type, x, y, dir, function (x, y) {
+        if ((x < 0) || (x >= width) || (y < 0) || (y >= height) || getBlock(x, y)) {
             result = true;
         }
     });
@@ -107,97 +107,97 @@ function playGame() {
         requestAnimationFrame(frame, CANVAS);
     }
     resize();
-    reset();  
-    frame();  
+    reset();
+    frame();
 }
 
 function resize() {
-    CANVAS.width = CANVAS.clientWidth;  
-    CANVAS.height = CANVAS.clientHeight; 
-    dx = CANVAS.width / width; 
-    dy = CANVAS.height / height; 
-    INVALID.board = true; 
-    INVALID.next = true; 
+    CANVAS.width = CANVAS.clientWidth;
+    CANVAS.height = CANVAS.clientHeight;
+    dx = CANVAS.width / width;
+    dy = CANVAS.height / height;
+    INVALID.board = true;
+    INVALID.next = true;
 }
 
 function keydown(evt) {
     evt.preventDefault();
     if (playing) {
-        switch(evt.keyCode) {
-            case 37:   
-                moves.push(DIR.LEFT);   
+        switch (evt.keyCode) {
+            case 37:
+                moves.push(DIR.LEFT);
                 break;
-            case 39: 
-                moves.push(DIR.RIGHT);  
+            case 39:
+                moves.push(DIR.RIGHT);
                 break;
-            case 38:     
-                moves.push(DIR.UP);     
+            case 38:
+                moves.push(DIR.UP);
                 break;
-            case 40:   
-                moves.push(DIR.DOWN);   
+            case 40:
+                moves.push(DIR.DOWN);
                 break;
         }
     }
 }
 
-function play() { 
+function play() {
     document.getElementById("text").textContent = "Good luck!";
     showTime();
-    reset();          
-    playing = true;  
+    reset();
+    playing = true;
 }
 
-function end() { 
-    setVisualScore(); 
+function end() {
+    setVisualScore();
     clearInterval(timer);
-    playing = false; 
+    playing = false;
     alert("Game Over!");
     if (confirm("Would you like to play again?") === true) {
         location.reload();
     }
 }
 
-function setVisualScore(n) { 
-    vscore = n || score; 
-    INVALID.score  = true; 
+function setVisualScore(n) {
+    vscore = n || score;
+    INVALID.score = true;
 }
 
-function setScore(n) { 
-    score = n; 
+function setScore(n) {
+    score = n;
     setVisualScore(n);
 }
 
-function setLines(n) { 
-    lines = n; 
-    step = Math.max(SPEED.min, SPEED.start - (SPEED.decrement*lines)); 
-    INVALID.lines  = true; 
+function setLines(n) {
+    lines = n;
+    step = Math.max(SPEED.min, SPEED.start - (SPEED.decrement * lines));
+    INVALID.lines = true;
 }
 
-function getBlock(x,y) { 
-    return (blocks && blocks[x] ? blocks[x][y] : null); 
+function getBlock(x, y) {
+    return (blocks && blocks[x] ? blocks[x][y] : null);
 }
 
-function setBlock(x,y,type) { 
-    blocks[x] = blocks[x] || []; 
-    blocks[x][y] = type; 
-    INVALID.board  = true; 
+function setBlock(x, y, type) {
+    blocks[x] = blocks[x] || [];
+    blocks[x][y] = type;
+    INVALID.board = true;
 }
 
-function currentTetro(tetro) { 
-    current = tetro || renderTetro(); 
-    INVALID.board  = true;    
+function currentTetro(tetro) {
+    current = tetro || renderTetro();
+    INVALID.board = true;
 }
 
-function nextTetro(tetro) { 
-    next = tetro || renderTetro(); 
-    INVALID.next   = true; 
+function nextTetro(tetro) {
+    next = tetro || renderTetro();
+    INVALID.next = true;
 }
-    
+
 function reset() {
     dt = 0;
     moves = [];
     blocks = [];
-    INVALID.board  = true; 
+    INVALID.board = true;
     setLines(0);
     setScore(0);
     currentTetro(next);
@@ -219,39 +219,39 @@ function update(idt) {
 }
 
 function handle(action) {
-    switch(action) {
-        case DIR.LEFT:  
-            move(DIR.LEFT);  
+    switch (action) {
+        case DIR.LEFT:
+            move(DIR.LEFT);
             break;
-        case DIR.RIGHT: 
-            move(DIR.RIGHT); 
+        case DIR.RIGHT:
+            move(DIR.RIGHT);
             break;
-        case DIR.UP:    
-            rotate();        
+        case DIR.UP:
+            rotate();
             break;
-        case DIR.DOWN:  
-            drop();          
+        case DIR.DOWN:
+            drop();
             break;
     }
 }
 
 function move(dir) {
     let x = current.x, y = current.y;
-    switch(dir) {
-        case DIR.RIGHT: 
-            x = x + 1; 
+    switch (dir) {
+        case DIR.RIGHT:
+            x = x + 1;
             break;
-        case DIR.LEFT:  
-            x = x - 1; 
+        case DIR.LEFT:
+            x = x - 1;
             break;
-        case DIR.DOWN:  
-            y = y + 1; 
+        case DIR.DOWN:
+            y = y + 1;
             break;
     }
     if (!filled(current.type, x, y, current.dir)) {
         current.x = x;
         current.y = y;
-        INVALID.board  = true; 
+        INVALID.board = true;
         return true;
     } else {
         return false;
@@ -262,14 +262,14 @@ function rotate() {
     let newdir = (current.dir == DIR.MAX ? DIR.MIN : current.dir + 1);
     if (!filled(current.type, current.x, current.y, newdir)) {
         current.dir = newdir;
-        INVALID.board  = true; 
+        INVALID.board = true;
     }
 }
 
 function drop() {
     if (!move(DIR.DOWN)) {
         score += 10;
-        eachblock(current.type, current.x, current.y, current.dir, function(x, y) {
+        eachblock(current.type, current.x, current.y, current.dir, function (x, y) {
             setBlock(x, y, current.type);
         });
         clearLines();
@@ -284,9 +284,9 @@ function drop() {
 
 function clearLines() {
     let x, y, complete, n = 0;
-    for(y = height; y > 0 ; --y) {
+    for (y = height; y > 0; --y) {
         complete = true;
-        for(x = 0 ; x < width ; ++x) {
+        for (x = 0; x < width; ++x) {
             if (!getBlock(x, y)) {
                 complete = false;
             }
@@ -297,23 +297,23 @@ function clearLines() {
             n++;
         }
         if (n > 0) {
-        setLines(lines + n); 
-        score += (100 * Math.pow(2, n-1)); 
+            setLines(lines + n);
+            score += (100 * Math.pow(2, n - 1));
         }
     }
 }
 
 function clearLine(n) {
     let x, y;
-    for(y = n ; y >= 0 ; --y) {
-        for(x = 0 ; x < width ; ++x)
-        setBlock(x, y, (y == 0) ? null : getBlock(x, y-1));
+    for (y = n; y >= 0; --y) {
+        for (x = 0; x < width; ++x)
+            setBlock(x, y, (y == 0) ? null : getBlock(x, y - 1));
     }
 }
 
 function draw() {
     CTX.save();
-    CTX.translate(0.5, 0.5); 
+    CTX.translate(0.5, 0.5);
     drawBoard();
     if (INVALID.score) {
         document.getElementById("score").textContent = ("000" + score);
@@ -329,31 +329,39 @@ function drawBoard() {
             drawTetro(CTX, current.type, current.x, current.y, current.dir);
         }
         let x, y, block;
-        for(y = 0 ; y < height; y++) {
-            for (x = 0 ; x < width ; x++) {
-                if (block = getBlock(x,y)) {
-                CTX.fillStyle = block.color;
-                CTX.fillRect(x*dx, y*dy, dx, dy);
-                CTX.strokeRect(x*dx, y*dy, dx, dy);
+        for (y = 0; y < height; y++) {
+            for (x = 0; x < width; x++) {
+                if (block = getBlock(x, y)) {
+                    CTX.fillStyle = block.color;
+                    CTX.fillRect(x * dx, y * dy, dx, dy);
+                    CTX.strokeRect(x * dx, y * dy, dx, dy);
                 }
             }
         }
-        CTX.strokeRect(0, 0, width*dx - 1, height*dy - 1); 
+        CTX.strokeRect(0, 0, width * dx - 1, height * dy - 1);
         INVALID.board = false;
     }
 }
 
 function drawTetro(ctx, type, x, y, dir) {
-    eachblock(type, x, y, dir, function(x, y) {
+    eachblock(type, x, y, dir, function (x, y) {
         ctx.fillStyle = type.color;
-        ctx.fillRect(x*dx, y*dy, dx, dy);
-        ctx.strokeRect(x*dx, y*dy, dx, dy);
+        ctx.fillRect(x * dx, y * dy, dx, dy);
+        ctx.strokeRect(x * dx, y * dy, dx, dy);
     });
 }
 
 playGame();
-document.getElementById("start").addEventListener("click", function() {
+document.getElementById("start").addEventListener("click", function () {
     if (!playing) {
         play();
-    } 
+    }
+});
+
+// materialize CSS side nav
+$('.button-collapse').sideNav({
+    menuWidth: 300,
+    edge: 'left',
+    closeOnClick: true,
+    draggable: true,
 });
